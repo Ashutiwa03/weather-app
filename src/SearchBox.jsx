77ca783004a5fadc886487b92d5207c9f95ebd24
Ashutosh Tiwari <at1181559@gmail.com>
2026-09-18 15:27:16 +0530

    Update gitignore

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -6,7 +6,7 @@ export default function SearchBox({updateInfo}){
      let[city,setCity] = useState("");
      let[error, setError] = useState(false);
     const API_URL="https://api.openweathermap.org/data/2.5/weather";
-    const API_KEY="REMOVED_API_KEY";
+    const API_KEY=import.meta.env.VITE_WEATHER_API_KEY;
 
     let getWeatherInfo = async() =>{
         try{
